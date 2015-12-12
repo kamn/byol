50419ce728ad17114b13186055a24fde4fae8607
Samuel Miller <kamn.miller@gmail.com>
2015-12-12 13:56:27 -0700

    Added some new functionality

--- prompt.c
+++ prompt.c
@@ -1,22 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//a buffer for input 
-static char input[2048];
+
+#ifdef _WIN32
+#define SYS_NAME "Windows"
+#elif __linux__
+#define SYS_NAME "Linux"
+#elif __unix__
+#define SYS_NAME "Unix"
+#else
+#define SYS_NAME "Unknown"
+#endif
+
+
+//Read history hack for windows
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+//Fake readline
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+//Fake add history
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
   
   //Version and Exit info
-  puts("Lispy verion 0.0.0.0.1");
+  puts("Risky verion 0.0.0.0.1");
+  printf("Running on %s\n", SYS_NAME);
   puts("Press Ctrl-c to Exit\n");
 
   while(1) {
-    //Out put prompt
-    fputs("lispy> ", stdout);
 
-    //Get user input
-    fgets(input, 2048, stdin);
+    //Output prompt and get input
+    char* input = readline("risky> ");
 
-    printf("No you're a %s", input);
+    //Add input history
+    add_history(input);
+
+    printf("%s\n", input);
+
+    //free memory
+    free(input);
   }
 
   return 0;
