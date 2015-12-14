5be30d4c072b68acfefa1b9a9b2f8d822c7a0375
Samuel Miller <kamn.miller@gmail.com>
2015-12-13 19:41:15 -0700

    Started to refactor from my own sanity

--- risky.c
+++ risky.c
@@ -71,6 +71,28 @@ typedef struct lval {
   struct lval** cell;
 } lval;
 
+/** =================
+Beginning of Pre-defs
+
+===================== */
+
+void lval_del(lval* v); 
+lval* lval_eval_sexpr(lenv* e, lval* v);
+lval* builtin_eval(lenv* e, lval* a);
+lval* lval_copy(lval* v);
+lval* lval_err(char* fmt, ...);
+
+/** =================
+End of Pre-defs
+
+===================== */
+
+
+/** =================
+Beginning of Enviroment Functions
+
+===================== */
+
 struct lenv {
 lenv* par;
   int count;
@@ -87,12 +109,71 @@ lenv* lenv_new(void) {
   return e;
 }
 
-lval* lenv_get(lenv* e, lval* k);
-void lval_del(lval* v); 
-lval* lval_eval_sexpr(lenv* e, lval* v);
-lenv* lenv_copy(lenv* e);
-void lenv_del(lenv* e);
-lval* builtin_eval(lenv* e, lval* a);
+lval* lenv_get(lenv* e, lval* k) {
+  for(int i = 0; i < e->count; i++) {
+    if(strcmp(e->syms[i], k->sym) == 0) {
+      return lval_copy(e->vals[i]);
+    }
+  }
+  
+  if(e->par) {
+    return lenv_get(e->par, k);
+  } else {
+    return lval_err("Unbound symbol");
+  }
+}
+
+lenv* lenv_copy(lenv* e) {
+  lenv* n = malloc(sizeof(lenv));
+  n->par = e->par;
+  n->count = e->count;
+  n->syms = malloc(sizeof(char*) * n->count);
+  n->vals = malloc(sizeof(lval*) * n->count);
+  for(int i = 0; i < e->count; i++) {
+    n->syms[i] = malloc(strlen(e->syms[i]) + 1);
+    strcpy(n->syms[i], e->syms[i]);
+    n->vals[i] = lval_copy(e->vals[i]);
+  }
+  return n;
+}
+
+void lenv_put(lenv* e, lval* k, lval* v) {
+  for(int i = 0; i < e->count; i++) {
+    if(strcmp(e->syms[i], k->sym) == 0) {
+      lval_del(e->vals[i]);
+      e->vals[i] = lval_copy(v);
+      return;
+    }
+  }
+
+  e->count++;
+  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
+  e->syms = realloc(e->syms, sizeof(char*) * e->count);
+
+  e->vals[e->count-1] = lval_copy(v);
+  e->syms[e->count-1] = malloc(strlen(k->sym)+1);
+  strcpy(e->syms[e->count-1], k->sym);
+}
+
+void lenv_def(lenv* e, lval* k, lval* v) {
+  while(e->par) {e = e->par; }
+  lenv_put(e, k, v);
+}
+
+void lenv_del(lenv* e) {
+  for(int i = 0; i < e->count; i++) {
+    free(e->syms[i]);
+    lval_del(e->vals[i]);
+  }
+  free(e->syms);
+  free(e->vals);
+  free(e);
+}
+
+/** =================
+End of Enviroment Functions
+
+===================== */
 
 int REPL = 1;
 
@@ -252,68 +333,6 @@ lval* lval_add(lval* v, lval* x) {
   v->cell[v->count-1] = x;
   return v;
 }
-
-lval* lenv_get(lenv* e, lval* k) {
-  for(int i = 0; i < e->count; i++) {
-    if(strcmp(e->syms[i], k->sym) == 0) {
-      return lval_copy(e->vals[i]);
-    }
-  }
-  
-  if(e->par) {
-    return lenv_get(e->par, k);
-  } else {
-    return lval_err("Unbound symbol");
-  }
-}
-
-lenv* lenv_copy(lenv* e) {
-  lenv* n = malloc(sizeof(lenv));
-  n->par = e->par;
-  n->count = e->count;
-  n->syms = malloc(sizeof(char*) * n->count);
-  n->vals = malloc(sizeof(lval*) * n->count);
-  for(int i = 0; i < e->count; i++) {
-    n->syms[i] = malloc(strlen(e->syms[i]) + 1);
-    strcpy(n->syms[i], e->syms[i]);
-    n->vals[i] = lval_copy(e->vals[i]);
-  }
-  return n;
-}
-
-void lenv_put(lenv* e, lval* k, lval* v) {
-  for(int i = 0; i < e->count; i++) {
-    if(strcmp(e->syms[i], k->sym) == 0) {
-      lval_del(e->vals[i]);
-      e->vals[i] = lval_copy(v);
-      return;
-    }
-  }
-
-  e->count++;
-  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
-  e->syms = realloc(e->syms, sizeof(char*) * e->count);
-
-  e->vals[e->count-1] = lval_copy(v);
-  e->syms[e->count-1] = malloc(strlen(k->sym)+1);
-  strcpy(e->syms[e->count-1], k->sym);
-}
-
-void lenv_def(lenv* e, lval* k, lval* v) {
-  while(e->par) {e = e->par; }
-  lenv_put(e, k, v);
-}
-
-void lenv_del(lenv* e) {
-  for(int i = 0; i < e->count; i++) {
-    free(e->syms[i]);
-    lval_del(e->vals[i]);
-  }
-  free(e->syms);
-  free(e->vals);
-  free(e);
-}
-
 
 lval* lval_read_num(mpc_ast_t* t) {
   errno = 0;
